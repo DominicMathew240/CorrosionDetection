6bc57b1deb78c9efe688478379416324815a606f
Dominic Mathew Anak David <112074315+DominicMathew240@users.noreply.github.com>
2023-12-28 17:16:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,6 @@ footer = f"""
 <div class="footer">
 <img src="data:image/png;base64,{node_comp_base64}" width="32%" class="footer-image"/>
 <img src="data:image/png;base64,{stratetics_base64}" width="32%" class="footer-image"/>
-# <img src="data:image/png;base64,{dronez_base64}" width="18%" class="footer-image"/>
 </div>
 """
 st.markdown(footer, unsafe_allow_html=True)
